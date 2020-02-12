3830913221beb33e6dbac358ac41acae6139a8f9
DeathTheBest <goosesuka@gmail.com>
2020-02-12 13:46:10 +0200

    autoslider
    autoslider

--- 1.js
+++ 1.js
@@ -1,13 +1,23 @@
-document.getElementById("slider-left").onclick = sliderLeft;
-
+// запуск по кнопке
+// document.getElementById("slider-left").onclick = sliderLeft; 
+// запуск по кнопке
+autoSlider();
 var left = 0;
+var timer;
 
-function sliderLeft () {
-    var polosa = document.getElementById("polosa");
-    left = left - 128;
-    if (left< -512) {
-        left = 0;
-    }
-    polosa.style.left = left +'px';
-
+function autoSlider () {
+    timer = setTimeout(function () {
+        var polosa = document.getElementById("polosa");
+        left = left - 128;
+        if (left< -512) {
+            left = 0;
+            clearTimeout(timer);
+        }
+        polosa.style.left = left +'px';
+        autoSlider ();
+    }, 1000);
 }
+
+
+
+
